c68ee1587c8055d575979de64dc4aafab7518daf
dnyaneshm36 <dnyaneshm36@gmail.com>
2020-12-31 11:54:47 +0530

    mongodb added

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    SECRET_KEY = '5f352379324c22463451387a0aec5d2f'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
